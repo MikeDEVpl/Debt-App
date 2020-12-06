1eaba68280fe8fadcb6e127b66d8fd429bd38234
MikeDevPl <46839276+MikeDEVpl@users.noreply.github.com>
2020-12-06 16:25:24 +0100

    Changed to hbs. Skeleton of app without mongo integration

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//Route dla sciezki users
+router.get('/users', function(req, res, next) {
+  res.render('index', { title: 'Users' });
+});
 module.exports = router;
