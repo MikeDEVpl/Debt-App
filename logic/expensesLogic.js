49f42c011442833bb27084f6205c9dfeabca22f2
MikeDevPl <46839276+MikeDEVpl@users.noreply.github.com>
2020-12-14 21:27:03 +0100

    added view logic for expenses and loans

---
+++ logic/expensesLogic.js
@@ -0,0 +1,99 @@
+// 1. Stworzenie listy zdarzen (docelowo za pomocą getAllEvents)
+// 2. Dla wybranego zdarzenia zwrocenie wydatkow w postaci listy (docelowo za pom getExpensesForEvent)
+
+let events = [
+    {
+        name: 'Wyjazd',
+        date: '2020-10-11',
+        participants: [ 
+            {
+                name: 'Michał',
+                surname: 'Kichał'
+            },
+            {
+                name: 'Jurek',
+                surname: 'Spurek'
+            }
+        ],
+        ended: true
+    },
+    {
+        name: 'Koncert',
+        date: '2020-08-22',
+        participants: [ 
+            {
+                name: 'Marcin',
+                surname: 'Arcin'
+            },
+            {
+                name: 'Kot',
+                surname: 'Mruczek'
+            }
+        ],
+        ended: false
+    }    
+];
+
+let expenses = [
+    {
+        name: 'Piwo',
+        cost: 200.00,
+        sponsoredBy:'Jerzy'
+    },  {
+        name: 'Grill',
+        cost: 75.00,
+        sponsoredBy:'Maciek'
+    }];
+    
+let selectEvent = document.getElementById("selectEvent");
+
+events.forEach(element => {
+    let newOption=document.createElement('option');
+    newOption.appendChild(document.createTextNode(element.name));
+    newOption.value=element.name;
+    selectEvent.appendChild(newOption);
+});
+
+
+//Tworzenie kart dla expense
+let cardContainer=document.getElementById("cardContainer");
+
+expenses.forEach(element => {
+    // div card
+    let newCard = document.createElement('div');
+    newCard.classList.add("card");
+    newCard.style='width: 8rem';
+
+    //card body
+    let newCardBody=document.createElement('div');
+    newCardBody.classList.add('card-body');
+    
+    //card title
+    let newCardTitle = document.createElement('h5');
+    newCardTitle.appendChild(document.createTextNode(element.name));
+    newCardTitle.classList.add('card-title');
+
+    //card-text
+    let newCardText = document.createElement('p');
+    newCardText.appendChild(document.createTextNode(element.sponsoredBy));
+    newCardText.classList.add('card-text');
+
+    //card button
+    let newCardButton = document.createElement('a');
+    newCardButton.classList.add('btn');
+    newCardButton.classList.add('btn-primary');
+    newCardButton.appendChild(document.createTextNode(element.cost));
+
+
+    newCardBody.appendChild(newCardTitle);
+    newCardBody.appendChild(newCardText);
+    newCardBody.appendChild(newCardButton);
+
+    newCard.appendChild(newCardBody);
+    cardContainer.appendChild(newCard);
+});
+
+
+
+
+
--- logic/loansLogic.js
+++ logic/loansLogic.js
@@ -1,10 +1,81 @@
-var $ = require('jquery');
+//Te loansy maja byc z repozytorium
+let loans = [
+    {
+        name: 'Edmund',
+        amount: 90.00,
+        returnDate: '9999-12-23',
+        paid: false
+    },
+    {
+        name: 'Wiktor',
+        amount: 15.00,
+        returnDate: '9999-11-09',
+        paid:true
+    }
+];
+
+//Logika widoku (tworzenie elementów)
+
+let tbody= document.getElementById("tableBody");
+let thead = document.getElementById("tableHead");
+
+//Create headers
+let newRowHead  = document.createElement("tr");
+
+for(let header of Object.keys(loans[0])){
+    let newTh = document.createElement("th");
+    newTh.innerText= header;
+    newRowHead.appendChild(newTh);
+    thead.appendChild(newRowHead);
+}
+
+//Create row for buttons
+let emptyColumn = document.createElement("th");
+emptyColumn.innerText="Rozlicz";
+newRowHead.appendChild(emptyColumn);
+thead.appendChild(newRowHead);
+
+
+//Create rows
+loans.forEach(element => {
+    let newRow=document.createElement('tr');
+
+    let newDebtorName=document.createElement('td');
+    newDebtorName.id='debtor';
+    newDebtorName.innerHTML=element.name;
+
+    let newAmount=document.createElement('td');
+    newAmount.id='amount';
+    newAmount.innerHTML=element.amount;
+
+    let newReturnDate=document.createElement('td');
+    newReturnDate.id='return-date';
+    newReturnDate.innerHTML=element.returnDate;
+
+    let newPaidFlag=document.createElement('td');
+    newPaidFlag.id='is-paid';
+    newPaidFlag.innerHTML=element.paid;
+
+    let newPayButton = document.createElement("a");
+    newPayButton.id="paid-btn";
+    let textPayButton = document.createTextNode("Rozlicz");
+    newPayButton.appendChild(textPayButton);
+    newPayButton.classList.add("btn"); 
+    newPayButton.classList.add("btn-primary");
+    newPayButton.classList.add("btn-sm");
+    newPayButton.classList.add("_table-row");
+    newPayButton.value="Rozlicz";
+
+    newRow.appendChild(newDebtorName);
+    newRow.appendChild(newAmount);
+    newRow.appendChild(newReturnDate);
+    newRow.appendChild(newPaidFlag);
+    newRow.appendChild(newPayButton);
+
+    tbody.appendChild(newRow);    
+});
 
 
 
-//Create rows with loans using repository GetAllLoans method
-//Użyć jquery
-//wg. todoapi
-let inputDebtor;
-let inputAmount;
-let inputReturnDate;
+
+
